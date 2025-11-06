5b54e572f8db8ad0dcb371d19cca3f75b127746a
Noran Diaconu <norandiaconu@gmail.com>
2025-11-05 19:08:17 -0500

    Updated ng command

--- ng.cmd
+++ ng.cmd
@@ -1,2 +1,16 @@
 @echo off
-node "%cd%\node_modules\@angular\cli\bin\ng.js" %*
+:loop
+if exist "%cd%\node_modules\@angular\cli\bin\ng.js" (
+    echo | set /p=%cd% 
+    node "%cd%\node_modules\@angular\cli\bin\ng.js" %*
+    goto :end
+) else (
+    if "%cd%"=="%~d0\" (
+        echo ng not found
+        goto :end
+    ) else (
+        cd ..
+    )
+)
+goto :loop
+:end
